26fa6d621eb94f39a462322602f07b0ec2939c62
TonyMo0310 <152178623+TonyMo0310@users.noreply.github.com>
2025-01-07 01:12:32 +0800

    添加题目

--- src/lab1/ass1.java
+++ src/lab1/ass1.java
@@ -47,3 +47,45 @@ public class ass1 {
         return arr;
     }
 }
+/*
+题目描述
+Given two arrays A
+ with length n
+ and B
+ with length T
+. We want to know whether each element in array B
+ is in array A
+ or not.
+
+输入
+The 1st line is a positive integer n(1≤n≤1000)
+
+The 2nd line contains n
+ integers: a1,a2,...,an.For each ai,0≤ai≤109
+.
+
+The 3rd line contains a positive integer T(1≤T≤1000)
+
+The 4th line contains T
+ integers b1,b2,...,bT.For each bi,0≤bi≤109
+.
+
+
+
+输出
+
+For each element in B
+, print "yes" (in a single line, without quotes) if it is in A
+, otherwise print "no"
+
+
+
+样例输入
+4
+2 3 999999 1
+2
+3 99
+样例输出
+yes
+no
+ */
--- src/lab1/ass2.java
+++ src/lab1/ass2.java
@@ -38,4 +38,40 @@ public class ass2 {
         out.close();
     }
 }
+/*
+题目描述
+Given two arrays A with length n and B with length T. We want to know whether each element in array B is in array A or not.
+
+
+
+Since the input is large, you may need fast I/O (in Java). Java Faster I/O is shown via following links: https://pastebin.ubuntu.com/p/zBd3g4j366/
+
+输入
+The 1st line is a positive integer n
+ (n≤106)
+
+The 2nd line contains n
+ integers: a1,a2...an.For each ai,0≤ai≤105
+
+The 3rd line contains a positive integer T(T≤106)
+
+The 4th line contains T
+ integers b1,b2,...bT.For each bi,0≤bi≤105
+
+输出
+For each element in B
+, print "yes" (in a single line, without quotes) if it is in A
+, otherwise print "no"
+样例输入
+4
+2 3 9999 1
+2
+3 99
+样例输出
+yes
+no
+提示
+
+Note that the integer value ranges are different in Problem 1 and 2.
+ */
 
